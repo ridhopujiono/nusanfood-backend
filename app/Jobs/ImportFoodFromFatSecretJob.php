34c10fe2f402900682aef3a20407ea5c49953c1d
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-12-31 16:00:20 +0700

    feat: Add missing food logging; use updateOrCreate

--- app/Jobs/ImportFoodFromFatSecretJob.php
+++ app/Jobs/ImportFoodFromFatSecretJob.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Jobs;
 
 use App\Models\Food;
@@ -8,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class ImportFoodFromFatSecretJob implements ShouldQueue
 {
@@ -25,6 +27,7 @@ class ImportFoodFromFatSecretJob implements ShouldQueue
         $searchResult = $fatSecret->searchFood($this->searchExpression);
 
         if (!isset($searchResult['foods']['food']['food_id'])) {
+            Log::info('No food found');
             return;
         }
 
@@ -36,6 +39,7 @@ class ImportFoodFromFatSecretJob implements ShouldQueue
         $foodDetail = $fatSecret->getFoodDetail($foodId);
 
         if (!isset($foodDetail['food'])) {
+            Log::info('No food detail found');
             return;
         }
 
@@ -60,36 +64,44 @@ class ImportFoodFromFatSecretJob implements ShouldQueue
         $servings = $foodData['servings']['serving'] ?? [];
 
         foreach ($servings as $serving) {
-            $foodServing = $food->servings()->create([
-                'serving_label' => $serving['serving_description'],
-                'metric_amount' => $serving['metric_serving_amount'] ?? null,
-                'metric_unit' => $serving['metric_serving_unit'] ?? null,
-                'household_amount' => $serving['number_of_units'] ?? null,
-                'household_unit' => $serving['measurement_description'] ?? null,
-            ]);
 
-            $foodServing->nutrition()->create([
-                'calories' => $serving['calories'] ?? null,
-                'carbohydrate' => $serving['carbohydrate'] ?? null,
-                'protein' => $serving['protein'] ?? null,
-                'fat' => $serving['fat'] ?? null,
+            $foodServing = $food->servings()->updateOrCreate(
+                [
+                    'serving_label' => $serving['serving_description'],
+                    'metric_amount' => $serving['metric_serving_amount'] ?? null,
+                    'metric_unit' => $serving['metric_serving_unit'] ?? null,
+                ],
+                [
+                    'household_amount' => $serving['number_of_units'] ?? null,
+                    'household_unit' => $serving['measurement_description'] ?? null,
+                ]
+            );
 
-                'saturated_fat' => $serving['saturated_fat'] ?? null,
-                'polyunsaturated_fat' => $serving['polyunsaturated_fat'] ?? null,
-                'monounsaturated_fat' => $serving['monounsaturated_fat'] ?? null,
+            $foodServing->nutrition()->updateOrCreate(
+                [],
+                [
+                    'calories' => $serving['calories'] ?? null,
+                    'carbohydrate' => $serving['carbohydrate'] ?? null,
+                    'protein' => $serving['protein'] ?? null,
+                    'fat' => $serving['fat'] ?? null,
 
-                'fiber' => $serving['fiber'] ?? null,
-                'sugar' => $serving['sugar'] ?? null,
+                    'saturated_fat' => $serving['saturated_fat'] ?? null,
+                    'polyunsaturated_fat' => $serving['polyunsaturated_fat'] ?? null,
+                    'monounsaturated_fat' => $serving['monounsaturated_fat'] ?? null,
 
-                'cholesterol' => $serving['cholesterol'] ?? null,
-                'sodium' => $serving['sodium'] ?? null,
-                'potassium' => $serving['potassium'] ?? null,
+                    'fiber' => $serving['fiber'] ?? null,
+                    'sugar' => $serving['sugar'] ?? null,
 
-                'vitamin_a' => $serving['vitamin_a'] ?? null,
-                'vitamin_c' => $serving['vitamin_c'] ?? null,
-                'calcium' => $serving['calcium'] ?? null,
-                'iron' => $serving['iron'] ?? null,
-            ]);
+                    'cholesterol' => $serving['cholesterol'] ?? null,
+                    'sodium' => $serving['sodium'] ?? null,
+                    'potassium' => $serving['potassium'] ?? null,
+
+                    'vitamin_a' => $serving['vitamin_a'] ?? null,
+                    'vitamin_c' => $serving['vitamin_c'] ?? null,
+                    'calcium' => $serving['calcium'] ?? null,
+                    'iron' => $serving['iron'] ?? null,
+                ]
+            );
         }
     }
 }
