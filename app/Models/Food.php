763526f83ab71e6779bf1987fed62e3040372d59
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-12-31 14:49:01 +0700

    feat: add Food model with fillable and servings

---
+++ app/Models/Food.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Food extends Model
+{
+    protected $fillable = [
+        'name',
+        'food_type',
+        'description',
+        'source',
+    ];
+
+    public function servings()
+    {
+        return $this->hasMany(FoodServing::class);
+    }
+}
