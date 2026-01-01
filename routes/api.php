ad1db49f738c3a826327a817ac5c70b69d66ef3e
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-01-01 23:01:56 +0700

    refactor: batch endpoint & restructure food routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BatchController;
+use App\Http\Controllers\Api\FoodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +14,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', function (Request $request) {
         return $request->user();
     });
+    Route::get('/batches/{id}', [BatchController::class, 'show']);
 
     // example protected route
+    Route::prefix('foods')->group(function () {
+        Route::post('/upload', [FoodController::class, 'uploadExcel']);
+        Route::post('/', [FoodController::class, 'store']);
+        Route::get('/', [FoodController::class, 'index']);
 
-    Route::apiResource('foods', App\Http\Controllers\Api\FoodController::class);
-    Route::post('/foods/{id}/verify', [App\Http\Controllers\Api\FoodController::class, 'verify']);
+        Route::get('{food}', [FoodController::class, 'show']);
+        Route::put('{food}', [FoodController::class, 'update']);
+        Route::delete('{food}', [FoodController::class, 'destroy']);
+
+
+
+        // khusus nutrisi
+        Route::put(
+            '{food}/servings/{serving}/nutrition',
+            [FoodController::class, 'updateNutrition']
+        );
+    });
 });
